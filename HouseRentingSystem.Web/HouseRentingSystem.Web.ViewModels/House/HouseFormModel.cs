f795ac10075a53417be926d8374f598eb93ecdeb
nikolaiandrov <nikolaiandrov@abv.bg>
2023-12-19 15:07:37 +0200

    View and house HTTPPOST models added

---
+++ HouseRentingSystem.Web/HouseRentingSystem.Web.ViewModels/House/HouseFormModel.cs
@@ -0,0 +1,38 @@
+﻿namespace HouseRentingSystem.Web.ViewModels.House
+{
+	using HouseRentingSystem.Web.ViewModels.Category;
+	using System.ComponentModel.DataAnnotations;
+	using static Common.EntityValidationConstants.HouseValidation;
+
+	public class HouseFormModel
+	{
+        public HouseFormModel()
+        {
+            this.Categories = new HashSet<CategoryFormModel>();
+        }
+
+		[Required(AllowEmptyStrings = false)]
+		[StringLength(TitleMaxLength, MinimumLength = TitleMinLength)]
+        public string Title { get; set; } = null!;
+
+		[Required(AllowEmptyStrings = false)]
+		[StringLength(AddressMaxLength, MinimumLength = AddressMinLength)]
+		public string Address { get; set; } = null!;
+
+		[Required(AllowEmptyStrings = false)]
+		[StringLength(DescriptionMaxLength, MinimumLength = DescriptionMinLength)]
+		public string Description { get; set; } = null!;
+
+		[Required(AllowEmptyStrings = false)]
+		[MaxLength(ImageUrlMaxlLength)]
+		public string ImageUrl { get; set; } = null!;
+
+		[Range(typeof(decimal), PricePerMonthMinValue, PricePerMonthMaxValue)]
+		public decimal PricePerMonth { get; set; }
+
+		[Required]
+		public int CategoryId { get; set; }
+
+		public ICollection<CategoryFormModel> Categories { get; set; }
+	}
+}
