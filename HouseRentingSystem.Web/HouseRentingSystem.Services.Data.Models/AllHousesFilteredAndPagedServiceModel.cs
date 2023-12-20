690098f82eceaa8dffa27b5f36abed39f96e5a37
nikolaiandrov <nikolaiandrov@abv.bg>
2023-12-20 14:56:48 +0200

    AllHousesFilteredAndPagedServiceModel - service model added

---
+++ HouseRentingSystem.Web/HouseRentingSystem.Services.Data.Models/AllHousesFilteredAndPagedServiceModel.cs
@@ -0,0 +1,16 @@
+﻿namespace HouseRentingSystem.Services.Data.Models
+{
+    using HouseRentingSystem.Web.ViewModels.House;
+
+	public class AllHousesFilteredAndPagedServiceModel
+	{
+        public AllHousesFilteredAndPagedServiceModel()
+        {
+            this.Houses = new HashSet<HouseAllViewModel>();
+        }
+
+        public int AllHousesCount { get; set; }
+
+		public ICollection<HouseAllViewModel> Houses { get; set; }
+	}
+}
