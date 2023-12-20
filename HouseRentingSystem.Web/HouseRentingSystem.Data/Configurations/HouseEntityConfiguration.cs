9b006a774706e9b73018819c9d14c1c5f540c124
nikolaiandrov <nikolaiandrov@abv.bg>
2023-12-20 17:31:38 +0200

    CreatedOn adjusted migration added

--- HouseRentingSystem.Web/HouseRentingSystem.Data/Configurations/HouseEntityConfiguration.cs
+++ HouseRentingSystem.Web/HouseRentingSystem.Data/Configurations/HouseEntityConfiguration.cs
@@ -8,9 +8,6 @@
     {
         public void Configure(EntityTypeBuilder<House> builder)
         {
-            builder.Property(h => h.CreatedOn)
-                .HasDefaultValue(DateTime.UtcNow);
-
             builder.HasOne(h => h.Category)
                 .WithMany(c => c.Houses)
                 .HasForeignKey(h => h.CategoryId)
@@ -37,6 +34,7 @@
                 Description = "A big house for your whole family. Don't miss to buy a house with three bedrooms.",
                 ImageUrl = "https://www.luxury-architecture.net/wp-content/uploads/2017/12/1513217889-7597-FAIRWAYS-010.jpg",
                 PricePerMonth = 2100.00M,
+                CreatedOn = DateTime.UtcNow,
                 CategoryId = 3,
                 AgentId = Guid.Parse("BE38A3D1-5835-4D2D-83FA-CA69DCBA5FB8"),
                 RenterId = Guid.Parse("85198CAA-C7E6-466F-2C80-08DBFF165C44")
@@ -50,6 +48,7 @@
                 Description = "It has the best comfort you will ever ask for. With two bedrooms, it is great for your family.",
                 ImageUrl = "https://cf.bstatic.com/xdata/images/hotel/max1024x768/179489660.jpg?k=2029f6d9589b49c95dcc9503a265e292c2cdfcb5277487a0050397c3f8dd545a&o=&hp=1",
                 PricePerMonth = 1200.00M,
+                CreatedOn = DateTime.UtcNow,
                 CategoryId = 2,
                 AgentId = Guid.Parse("BE38A3D1-5835-4D2D-83FA-CA69DCBA5FB8")
             };
@@ -62,6 +61,7 @@
                 Description = "This luxurious house is everything you will need. It is just excellent.",
                 ImageUrl = "https://i.pinimg.com/originals/a6/f5/85/a6f5850a77633c56e4e4ac4f867e3c00.jpg",
                 PricePerMonth = 2000.00M,
+                CreatedOn = DateTime.UtcNow,
                 CategoryId = 2,
                 AgentId = Guid.Parse("BE38A3D1-5835-4D2D-83FA-CA69DCBA5FB8")
             };
